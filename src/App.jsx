5304c0e5538d89593443db1c4fcc6f39bc05858c
Dmitrii <Dokter322@yandex.ru>
2023-01-08 17:32:01 +0300

    prototype

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,58 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
+import { useState, useEffect } from "react";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [input, setInput] = useState("");
+  const initialText =
+    "Я говорю вам: нужно носить в себе еще хаос, чтобы быть в состоянии родить танцующую звезду. Я говорю вам: в вас есть еще хаос.";
+  const text = initialText.split("");
+
+  let indexDone = input.length;
+  const textDone = text.slice(0, indexDone);
+  const [mistakeIndex, setMistakeIndex] = useState(null);
+
+  const handleInput = (e) => {
+    setInput(e.target.value);
+  };
+
+  useEffect(() => {
+    if (input != textDone.join("")) {
+      setMistakeIndex(textDone.join("").length);
+    } else {
+      setMistakeIndex(null);
+    }
+  }, [input]);
 
   return (
     <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div className="main-container">
+        <input type="text" value={input} onChange={(e) => handleInput(e)} />
+        <div className="text-container">
+          {textDone.map((letter, index) => {
+            console.log(mistakeIndex - 1, index);
+            // return mistakeIndex === null ||
+            //   (mistakeIndex - 1 !== index && mistakeIndex - 1 > index) ? (
+            //   <div className="letterDone">{letter}</div>
+            // ) : (
+            //   <div className="letterError">{letter}</div>
+            // );
+            return mistakeIndex === null ? (
+              <div className="letterDone">{letter}</div>
+            ) : mistakeIndex - 1 === index ? (
+              <div className="letterError">{letter}</div>
+            ) : mistakeIndex - 1 > index ? (
+              <div className="letterDone">{letter}</div>
+            ) : (
+              <h1>222</h1>
+            );
+          })}
+          {text.slice(indexDone).map((letter) => {
+            return <div className="letter">{letter}</div>;
+          })}
+        </div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
